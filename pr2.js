0da6866f0e807506154ffb99ed7f4d8486695e6b
janvimf <2024ucp1902>
2026-02-01 12:52:12 +0530

    added comments into the code

--- pr2.js
+++ pr2.js
@@ -1,13 +1,15 @@
-const form= document.querySelector('form')
+const form= document.querySelector('form')  
 form.addEventListener('submit', function(e)
 {
-    e.preventDefault();
+    e.preventDefault();   
+/* without preventDefualt() the page will be refreshed once we clicke submit 
+and we will not see the results. */
 
-    const height =parseInt(document.querySelector('#height').value)
-    const weight=parseInt(document.querySelector('#weight').value)
+    const height =parseInt(document.querySelector('#height').value)  //# cuz height is a id dats why #height ....for class its .classname 
+    const weight=parseInt(document.querySelector('#weight').value)  //parseint converts string to int ....cuz here we always get string
     const results=document.querySelector('#results');
 
-if(height==='' || isNaN(height) || height<0)
+if(height==='' || isNaN(height) || height<0)        //is not a number
 {results.innerHTML=`pls give valid height ${height}`}
 else if(weight==='' || isNaN(weight) || weight<0)
 {results.innerHTML=`pls give valid weight ${weight}`}
@@ -16,3 +18,19 @@ else{
     results.innerHTML=`<span>${bmi}</span>`
 }}
 );
+
+/* diff between innerHTML , innertext and textcontent 
+innerHTL reads the html tags . but innertext and textcontent 
+dont. 
+eg:-
+<div id="box">
+  Hello
+  <span style="display:none">Secret</span>
+</div>
+
+const box = document.quesryselector('#box')
+box.innertext  -----> hello    respects na dreads the css ...display:none 
+it is slower cud it reads css instrutions 
+
+box.textcontent ----> hello secret 
+faster cuz prints everything. 
